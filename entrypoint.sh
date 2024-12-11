be043f67387090f0de02af85e875237eef6ac0e1
Viet Nguyen <vietnguyenswin@outlook.com>
2024-12-12 00:14:59 +1100

    update entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-poetry run gunicorn --bind 0.0.0.0:8000 --worker-class gevent --timeout "${GUNICORN_TIMEOUT:-3600}" --workers "${GUNICORN_WORKERS_NUM:-4}" data_access_service.run:app
+poetry run gunicorn --bind 0.0.0.0:8000 --worker-class=gevent --timeout=3600 --workers=4 data_access_service.run:app
