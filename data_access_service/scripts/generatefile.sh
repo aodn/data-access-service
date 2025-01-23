870c29e09c700b3c642e780227e71bd7cce83dbd
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-01-24 09:42:28 +1100

    update generating file task

--- data_access_service/scripts/generatefile.sh
+++ data_access_service/scripts/generatefile.sh
@@ -7,12 +7,12 @@ import os
 from data_access_service.tasks.task import generate_csv_data_file
 
 uuid = os.getenv('UUID')
-start_time = os.getenv('START_TIME')
-end_time = os.getenv('END_TIME')
-min_lat = os.getenv('SOUTH')
-max_lat = os.getenv('NORTH')
-min_lon = os.getenv('WEST')
-max_lon = os.getenv('EAST')
+start_time = os.getenv('START_DATE')
+end_time = os.getenv('END_DATE')
+min_lat = os.getenv('MIN_LAT')
+max_lat = os.getenv('MAX_LAT')
+min_lon = os.getenv('MIN_LON')
+max_lon = os.getenv('MAX_LON')
 
 generate_csv_data_file(uuid, start_time, end_time, min_lat, max_lat, min_lon, max_lon)
 "
--- data_access_service/tasks/task.py
+++ data_access_service/tasks/task.py
@@ -1,17 +1,20 @@
-# @restapi.route("/data/csv", methods=["POST"])
 import boto3
-import pandas as pd
+
+from data_access_service import API
+from data_access_service.config.config import load_config
 
 
-def generate_csv_data_file(uuid, start_time, end_time, min_lat, max_lat, min_lon, max_lon):
+def generate_csv_data_file(uuid, start_date, end_date, min_lat, max_lat, min_lon, max_lon):
 
-    # for debug usage
+    config = load_config()
+
+    # for debug usage. just keep it for several days
     if uuid is None:
         uuid = "debug-uuid"
-    if start_time is None:
-        start_time = "debug-start-time"
-    if end_time is None:
-        end_time = "debug-end-time"
+    if start_date is None:
+        start_date = "debug-start-date"
+    if end_date is None:
+        end_date = "debug-end-date"
     if min_lat is None:
         min_lat = "debug-min-lat"
     if max_lat is None:
@@ -22,26 +25,32 @@ def generate_csv_data_file(uuid, start_time, end_time, min_lat, max_lat, min_lon
         max_lon = "debug-max-lon"
 
 
-    if None in [uuid, start_time, end_time, min_lat, max_lat, min_lon, max_lon]:
+    if None in [uuid, start_date, end_date]:
         raise ValueError("One or more required arguments are None")
 
-    print("start" + uuid)
-    data = {
-        "start_time": [start_time],
-        "end_time": [end_time],
-        "min_lat": [min_lat],
-        "max_lat": [max_lat],
-        "min_lon": [min_lon],
-        "max_lon": [max_lon],
-    }
-    data_frame = pd.DataFrame(data)
+    print("start " + uuid)
 
-    csv_file_path = "example1.csv"
+    api = API()
+
+    data_frame = api.get_dataset_data(
+        uuid=uuid,
+        date_start=start_date,
+        date_end=end_date,
+        lat_min=min_lat,
+        lat_max=max_lat,
+        lon_min=min_lon,
+        lon_max=max_lon,
+    )
+
+    if data_frame is None or data_frame.empty:
+        raise ValueError("No data found for the given parameters")
+
+    csv_file_path = f"lat:{min_lat}~{max_lat}_lon:{min_lon}~{max_lon}_date:{start_date}~{end_date}.csv"
     data_frame.to_csv(csv_file_path, index=False)
 
     s3 = boto3.client("s3")
-    bucket_name = "havier-example-bucket"
-    s3_path = f"example/{uuid}/example1.csv"
+    bucket_name = config["aws"]["s3"]["bucket_name"]["csv"]
+    s3_path = f"{uuid}/{csv_file_path}"
     try:
         s3.upload_file(csv_file_path, bucket_name, s3_path)
         print(f"File uploaded to s3://{bucket_name}/{s3_path}")
