6a2a497630320537ae9d87e0680c0878faeedddd
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-01-24 09:41:31 +1100

    add config file

--- data_access_service/config/config.py
+++ data_access_service/config/config.py
@@ -1,6 +1,8 @@
 import logging
 from enum import Enum
 
+import yaml
+
 
 class EnvType(Enum):
     DEV = "dev"
@@ -31,3 +33,8 @@ class StagingConfig(Config):
 class ProdConfig(Config):
     DEBUG = False
     LOGLEVEL = logging.INFO
+
+def load_config(file_path="config.yaml"):
+    with open(file_path, "r") as file:
+        config = yaml.safe_load(file)
+    return config
