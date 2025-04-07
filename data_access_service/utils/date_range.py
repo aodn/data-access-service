4a3cfc6613dc6da5d8dca924ca44c7bf0d969951
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-04-07 17:02:14 +1000

    update DateRange class

--- data_access_service/utils/date_range.py
+++ data_access_service/utils/date_range.py
@@ -1,13 +1,15 @@
 from datetime import datetime
 
 class DateRange:
-    def __init__(self, start_date: str, end_date: str, date_format: str = "%Y-%m-%d"):
-        self.start_date = datetime.strptime(start_date, date_format)
-        self.end_date = datetime.strptime(end_date, date_format)
+    def __init__(self, start_date: datetime, end_date: datetime, date_format: str = "%Y-%m-%d"):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.date_format = date_format
 
     def __str__(self):
-        return f"DateRange(start_date={self.start_date}, end_date={self.end_date})"
+        return f"{self.start_date.strftime(self.date_format)} - {self.end_date.strftime(self.date_format)}"
 
-    def contains(self, date: str, date_format: str = "%Y-%m-%d") -> bool:
-        date_obj = datetime.strptime(date, date_format)
-        return self.start_date <= date_obj <= self.end_date
+    def __eq__(self, other):
+        if not isinstance(other, DateRange):
+            return False
+        return self.start_date == other.start_date and self.end_date == other.end_date
