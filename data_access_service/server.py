beada75c9f992ed24cfbc32aae8caf86dedc2bea
rng <manfai.ng@utas.edu.au>
2025-05-02 17:40:42 +1000

    add service run

--- data_access_service/server.py
+++ data_access_service/server.py
@@ -1,5 +1,7 @@
 from contextlib import asynccontextmanager
+from pathlib import Path
 
+import uvicorn
 from fastapi import FastAPI
 from data_access_service.core.api import API
 from data_access_service.core.routes import router as api_router
@@ -14,3 +16,12 @@ async def lifespan(application: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan, title="Data Access Service")
+
+
+if __name__ == "__main__":
+    log_config_path = str(Path(__file__).parent.parent / "log_config.yaml")
+    uvicorn.run("data_access_service.server:app",
+                host="0.0.0.0",
+                port=8000,
+                reload=True,
+                log_config=log_config_path)
