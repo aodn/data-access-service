82d3501bfb6b7c1a9e79181e7011f20ed7c8a4d0
rng <manfai.ng@utas.edu.au>
2025-05-02 16:17:04 +1000

    Fixed test cases

--- tests/core/test_routes.py
+++ tests/core/test_routes.py
@@ -1,39 +1,35 @@
 import unittest
 from unittest.mock import MagicMock, patch
 from http import HTTPStatus
-from data_access_service.server import create_app
 from fastapi.testclient import TestClient
+from data_access_service.server import app
 
 
 class TestRoutes(unittest.TestCase):
     @patch("data_access_service.server.API")
-    def test_health_check_ready(self, MockAPI):
+    def test_health_check_ready(self, mock_api):
         mock_instance = MagicMock()
         mock_instance.get_api_status.return_value = True
-        MockAPI.return_value = mock_instance
+        mock_api.return_value = mock_instance
 
-        app = create_app()
-        client = TestClient(app)
-
-        response = client.get("/api/v1/das/health")
-        self.assertEqual(
-            response.json(), {"status": "UP", "status_code": HTTPStatus.OK}
-        )
+        with TestClient(app) as client:
+            response = client.get("/api/v1/das/health")
+            self.assertEqual(
+                response.json(), {"status": "UP", "status_code": HTTPStatus.OK}
+            )
 
     @patch("data_access_service.server.API")
-    def test_health_check_not_ready(self, MockAPI):
+    def test_health_check_not_ready(self, mock_api):
         mock_instance = MagicMock()
         mock_instance.get_api_status.return_value = False
-        MockAPI.return_value = mock_instance
+        mock_api.return_value = mock_instance
 
-        app = create_app()
-        client = TestClient(app)
-
-        response = client.get("/api/v1/das/health")
-        self.assertEqual(
-            response.json(),
-            {"status": "STARTING", "status_code": HTTPStatus.SERVICE_UNAVAILABLE},
-        )
+        with TestClient(app) as client:
+            response = client.get("/api/v1/das/health")
+            self.assertEqual(
+                response.json(),
+                {"status": "STARTING", "status_code": HTTPStatus.SERVICE_UNAVAILABLE},
+            )
 
 
 if __name__ == "__main__":
