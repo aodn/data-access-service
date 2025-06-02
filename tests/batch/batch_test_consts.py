9869f60d6dd1ac67c506a641af9c774c7452df19
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-06-02 10:58:06 +1000

    change the test consts

--- tests/batch/batch_test_consts.py
+++ tests/batch/batch_test_consts.py
@@ -5,8 +5,8 @@ INIT_JOB_ID = "init-job-id"
 
 INIT_PARAMETERS = {
     "uuid": "af5d0ff9-bb9c-4b7c-a63c-854a630b6984",
-    "start_date": "02-2014",
-    "end_date": "04-2015",
+    "start_date": "02-2010",
+    "end_date": "04-2011",
     "multi_polygon": '{"type":"MultiPolygon","coordinates":[[[[-180,90],[-180,-90],[180,-90],[180,90],[-180,90]]]]}',
     "recipient": "test-recipient",
 }
@@ -14,7 +14,7 @@ INIT_PARAMETERS = {
 PREPARATION_PARAMETERS = {
     **INIT_PARAMETERS,
     "type": "sub-setting-data-preparation",
-    'date_ranges': '{"0": ["2014-02-01", "2014-04-30"], "1": ["2014-05-01", "2014-07-31"], "2": ["2014-08-01", "2014-10-31"], "3": ["2014-11-01", "2015-01-31"], "4": ["2015-02-01", "2015-04-30"]}'
+    'date_ranges': '{"0": ["2010-02-01", "2010-04-30"], "1": ["2010-05-01", "2010-07-31"], "2": ["2010-08-01", "2010-10-31"], "3": ["2010-11-01", "2011-01-31"], "4": ["2011-02-01", "2011-04-30"]}'
 }
 
 PREPARATION_JOB_SUBMISSION_ARGS = {
@@ -27,9 +27,8 @@ PREPARATION_JOB_SUBMISSION_ARGS = {
 }
 
 COLLECTION_PARAMETERS = {
-    **INIT_PARAMETERS,
+    **PREPARATION_PARAMETERS,
     "type": "sub-setting-data-collection",
-    'date_ranges': '{"0": ["2014-02-01", "2014-04-30"], "1": ["2014-05-01", "2014-07-31"], "2": ["2014-08-01", "2014-10-31"], "3": ["2014-11-01", "2015-01-31"], "4": ["2015-02-01", "2015-04-30"]}'
 }
 
 COLLECTION_JOB_SUBMISSION_ARGS = {
