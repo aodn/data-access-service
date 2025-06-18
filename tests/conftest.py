cceacc72200fb543de262e8f146431bb8371bb05
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-06-18 13:08:54 +1000

    add a test global config

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+from unittest.mock import patch
+
+@pytest.fixture(autouse=True)
+def mock_aws_client():
+    print("Mocking AWSClient for tests")
+    with patch("data_access_service.core.AWSClient.AWSClient") as mock:
+        yield mock
