4445caafd1e3458e59214738d35adf0527d3f59d
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-09-11 17:19:55 +1000

    trim date range

--- data_access_service/batch/subsetting.py
+++ data_access_service/batch/subsetting.py
@@ -1,8 +1,14 @@
 import json
 
-from data_access_service import init_log, Config, API
+from data_access_service import init_log, Config
 from data_access_service.batch.batch_enums import Parameters
+from data_access_service.batch.subsetting_helper import (
+    get_uuid,
+    get_keys,
+    trim_date_range_for_keys,
+)
 from data_access_service.core.AWSHelper import AWSHelper
+from data_access_service.server import api_setup, app
 from data_access_service.tasks.data_collection import collect_data_files
 from data_access_service.tasks.generate_dataset import process_data_files
 from data_access_service.utils.date_time_utils import (
@@ -10,20 +16,6 @@ from data_access_service.utils.date_time_utils import (
     split_date_range,
     parse_date,
 )
-
-
-def _get_key(parameters) -> list[str]:
-    if (
-        Parameters.KEY.value in parameters
-        and parameters[Parameters.KEY.value] is not None
-    ):
-        return [item.strip() for item in parameters[Parameters.KEY.value].split(",")]
-    else:
-        return ["*"]
-
-
-def _get_uuid(parameters) -> str:
-    return parameters[Parameters.UUID.value]
 
 
 # The only purpose is to create suitable number of child job, we can fine tune the value
@@ -37,6 +29,20 @@ def init(job_id_of_init, parameters):
     end_date_str = parameters[Parameters.END_DATE.value]
 
     requested_start_date, requested_end_date = supply_day(start_date_str, end_date_str)
+    api = api_setup(app)
+    uuid = get_uuid(parameters)
+    keys = get_keys(parameters)
+    if "*" in keys:
+        md = api.get_mapped_meta_data(uuid)
+        keys = list(md.keys())
+
+    requested_start_date, requested_end_date = trim_date_range_for_keys(
+        uuid=uuid,
+        keys=keys,
+        requested_start_date=requested_start_date,
+        requested_end_date=requested_end_date,
+    )
+
     date_ranges = split_date_range(
         start_date=requested_start_date,
         end_date=requested_end_date,
@@ -84,11 +90,11 @@ def prepare_data(job_index: str | None, parameters):
     logger = init_log(config)
 
     # get params
-    uuid = _get_uuid(parameters)
+    uuid = get_uuid(parameters)
     # An uuid can host multiple data file, so we need a key
     # to narrow our target file. Right now it will be the filename
     # if nothing specified, assume all files taken
-    key = _get_key(parameters)
+    key = get_keys(parameters)
     master_job_id = parameters[Parameters.MASTER_JOB_ID.value]
     date_ranges = parameters[Parameters.DATE_RANGES.value]
     date_ranges_dict = json.loads(date_ranges)
---
+++ data_access_service/batch/subsetting_helper.py
@@ -0,0 +1,50 @@
+from typing import List
+
+import pandas as pd
+
+from data_access_service.batch.batch_enums import Parameters
+from data_access_service.server import api_setup, app
+
+
+def get_keys(parameters) -> list[str]:
+    if (
+        Parameters.KEY.value in parameters
+        and parameters[Parameters.KEY.value] is not None
+    ):
+        return [item.strip() for item in parameters[Parameters.KEY.value].split(",")]
+    else:
+        return ["*"]
+
+
+def get_uuid(parameters) -> str:
+    return parameters[Parameters.UUID.value]
+
+
+def trim_date_range_for_keys(
+    uuid: str,
+    keys: List[str],
+    requested_start_date: pd.Timestamp,
+    requested_end_date: pd.Timestamp,
+) -> tuple[pd.Timestamp, pd.Timestamp]:
+    api = api_setup(app)
+
+    min_date_of_keys = pd.Timestamp.now()
+    max_date_of_keys = pd.Timestamp("1970-01-01 00:00:00.000000000")
+
+    # get the union spatial extents of all selected keys
+    for key in keys:
+        start_date, end_date = api.get_temporal_extent(uuid, key)
+        if start_date < min_date_of_keys:
+            min_date_of_keys = start_date
+        if end_date > max_date_of_keys:
+            max_date_of_keys = end_date
+
+    # if the requested dates are outside the available range, trim them
+    trimmed_start_date = requested_start_date
+    trimmed_end_date = requested_end_date
+    if requested_start_date < min_date_of_keys:
+        trimmed_start_date = min_date_of_keys
+    if requested_end_date > max_date_of_keys:
+        trimmed_end_date = max_date_of_keys
+
+    return trimmed_start_date, trimmed_end_date
