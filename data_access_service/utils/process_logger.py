6b6cd6713763e01a7a66512b53ff89dcfcbc5dfc
HuaizhiDai <huaizhi.dai@utas.edu.au>
2025-09-23 11:09:55 +1000

    reduce process logs

--- data_access_service/utils/process_logger.py
+++ data_access_service/utils/process_logger.py
@@ -2,22 +2,44 @@ from dask.callbacks import Callback
 
 
 class ProcessLogger(Callback):
-    def __init__(self, logger):
+    def __init__(self, logger, task_name: str = "Dask Task"):
         self.logger = logger
         self.total_tasks = 0
         self.completed_tasks = 0
+        self.task_name = task_name
+        self.milestones = {
+            1,
+            2,
+            3,
+            4,
+            5,
+            10,
+            20,
+            30,
+            40,
+            50,
+            60,
+            70,
+            80,
+            90,
+            100,
+        }
+        self.logged_milestones = set()
 
     def _start(self, dsk):
         self.total_tasks = len(dsk)
         self.completed_tasks = 0
-        self.logger.info("Dask computation started.")
+        self.logged_milestones.clear()
+        self.logger.info(f"{self.task_name} started.")
 
     def _posttask(self, key, result, dsk, state, id):
         self.completed_tasks += 1
-        percent = (self.completed_tasks / self.total_tasks) * 100
-        self.logger.info(
-            f"Progress: {percent:.2f}% ({self.completed_tasks}/{self.total_tasks})"
-        )
+        percent = int((self.completed_tasks / self.total_tasks) * 100)
+        if percent in self.milestones and percent not in self.logged_milestones:
+            self.logger.info(
+                f"{self.task_name} progress: {percent}% ({self.completed_tasks}/{self.total_tasks})"
+            )
+            self.logged_milestones.add(percent)
 
     def _finish(self, dsk, state, errored):
-        self.logger.info("Dask computation finished.")
+        self.logger.info(f" {self.task_name} finished.")
