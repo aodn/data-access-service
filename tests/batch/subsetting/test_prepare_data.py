a3dbc555b73e2be74fb7f01bfd12cda58efb9c7e
rng <manfai.ng@utas.edu.au>
2025-10-19 11:47:04 +1100

    Add test to verify bounding polygon works

--- tests/batch/subsetting/test_prepare_data.py
+++ tests/batch/subsetting/test_prepare_data.py
@@ -1,6 +1,7 @@
 import json
 import shutil
 
+import numpy as np
 import pandas as pd
 import pytest
 import tempfile
@@ -168,8 +169,11 @@ class TestDataGeneration(TestWithS3):
         self, aws_clients, setup_resources, upload_test_case_to_s3
     ):
         """
-        Test with satellite data where lat/lon is of range [0,360]. We always assume on interface [-180,180] and this
-        test verify the code automatically handle it.
+        Test with satellite data where lat/lon is of range [0,360]. The dataset use in the test is specially crafted
+        that the lon is > 180., so any query < 180 will yield no result
+
+        We always assume opi interface [-180,180] and this test verify the code automatically handle it.
+
         :param aws_clients: The aws client to local s3
         :param setup_resources: Depends, just make sure resource setup correctly
         :param upload_test_case_to_s3: Make sure data uploaded to local s3
@@ -201,6 +205,61 @@ class TestDataGeneration(TestWithS3):
 
             with patch.object(AWSHelper, "send_email") as mock_send_email:
                 try:
+                    # First test, no bounding box so system will auto assign -180, 180. Internally if code correct
+                    # will result in value found from range > 180 because this is satellite data where system should
+                    # adjust -180, 180 to 0, 360 based on metadata's lon range in the dataset
+                    prepare_data(api, job_index="0", parameters=parameters)
+
+                    # Now open the file and inspect values
+                    store = xarray.open_mfdataset(
+                        paths=f"{temp_folder}/{parameters[Parameters.KEY.value]}/part-*.zarr",
+                        engine="zarr",
+                        combine="nested",
+                        consolidated=False,  # Must be false as the file is not consolidated_metadata()
+                        parallel=False,
+                    )
+
+                    assert (
+                        store.lon.size > 0
+                    ), "This dataset contains lon > 180, if code right we should get value with -180, 180 bounds"
+
+                    u = np.unique(store.lon.values)
+                    assert 182.25 in u, "Values greate than 182.25 found"
+                    assert 190.0 in u, "Values greate than 190.0 found"
+
+                    shutil.rmtree(
+                        f"{temp_folder}/{parameters[Parameters.KEY.value]}"
+                    )  # Clear up before run
+
+                    # Second test we specify a polygon range from -180, 0, it should translated to 0, 180, due to our crafted data
+                    # we should yield no result as above shows data range is 180 above.
+                    parameters[Parameters.MULTI_POLYGON.value] = json.dumps(
+                        obj={
+                            "type": "MultiPolygon",
+                            "coordinates": [
+                                # POLYGON 1: Northern Hemisphere (-180 to 0)
+                                [
+                                    [
+                                        [-180, 90],  # NW
+                                        [0, 90],  # NE
+                                        [0, 0],  # SE
+                                        [-180, 0],  # SW
+                                        [-180, 90],  # Close
+                                    ]
+                                ],
+                                # POLYGON 2: Southern Hemisphere (-180 to 0)
+                                [
+                                    [
+                                        [-180, 0],  # NW
+                                        [0, 0],  # NE
+                                        [0, -90],  # SE
+                                        [-180, -90],  # SW
+                                        [-180, 0],  # Close
+                                    ]
+                                ],
+                            ],
+                        }
+                    )
                     prepare_data(api, job_index="0", parameters=parameters)
 
                     # Now open the file and inspect values
@@ -214,8 +273,8 @@ class TestDataGeneration(TestWithS3):
                     log.info(store)
 
                     assert (
-                        store.lon.size > 0
-                    ), "This dataset contains lon > 180, if code right we should get value with -180, 180 bounds"
+                        store.lon.size == 0
+                    ), "This dataset contains lon > 180, if code right we should not get value"
 
                 except Exception as e:
                     assert False, f"prepare_data raised an exception: {e}"
