b0f76c04dff4208e238efc75fc6da400436157bc
Yuxuan Hu <lydiahu_hn@hotmail.com>
2026-01-07 11:03:51 +1100

    fix TestWithS3 no attribute error

--- tests/core/test_with_s3.py
+++ tests/core/test_with_s3.py
@@ -160,7 +160,9 @@ class TestWithS3:
         # Other test may have call this get_s3_filesystem() this function is cached and
         # may use different ENDPOINT_URL other than the one above
         # so we need to clear it now before the next call happens
-        DataQuery.get_s3_filesystem.cache_clear()
+
+        # temparary commented this line as current cloud_optimised library does not support this
+        # DataQuery.get_s3_filesystem.cache_clear()
         # Force a load so its cache value use the test value
         DataQuery.get_s3_filesystem()
 
