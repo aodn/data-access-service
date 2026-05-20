c57ccf36ef7096e786d5d8914e66914a838f2e11
rng <manfai.ng@utas.edu.au>
2026-05-20 17:02:27 +1000

    Fix test fail

--- tests/batch/subsetting/test_prepare_data.py
+++ tests/batch/subsetting/test_prepare_data.py
@@ -310,77 +310,6 @@ class TestDataGeneration(TestWithS3):
                     )
 
     @patch("aodn_cloud_optimised.lib.DataQuery.REGION", REGION)
-    def test_non_specified_multi_polygon(
-        self, aws_clients, setup_resources, upload_test_case_to_s3
-    ):
-        # Use a different master_job_id to avoid conflicts with other tests
-        parameters = PREPARATION_PARAMETERS.copy()
-        parameters[Parameters.MASTER_JOB_ID.value] = "998"
-        parameters[Parameters.MULTI_POLYGON.value] = "non-specified"
-
-        s3_client, _, _ = aws_clients
-        config = Config.get_config()
-        config.set_s3_client(s3_client)
-        helper = AWSHelper()
-
-        with patch.object(AWSHelper, "send_email") as mock_send_email:
-            try:
-                # List objects in S3
-                response = s3_client.list_objects_v2(
-                    Bucket=DataQuery.BUCKET_OPTIMISED_DEFAULT,
-                    Prefix=DataQuery.ROOT_PREFIX_CLOUD_OPTIMISED_PATH,
-                    Delimiter="/",
-                )
-
-                folders = [
-                    prefix["Prefix"][len(prefix) - 1 :]
-                    for prefix in response.get("CommonPrefixes", [])
-                    if prefix["Prefix"].endswith(".parquet/")
-                ]
-
-                assert len(folders) == 3
-                assert folders[1] == "animal_acoustic_tracking_delayed_qc.parquet/"
-
-                # Verify DataQuery functionality
-                aodn = DataQuery.GetAodn()
-                metadata: Metadata = aodn.get_metadata()
-                assert (
-                    metadata.metadata_catalog().get(
-                        "animal_acoustic_tracking_delayed_qc.parquet"
-                    )
-                    is not None
-                )
-
-                # prepare data according to the test parameters
-                api = API()
-                api.initialize_metadata()
-                for i in range(5):
-                    prepare_data(api, job_index=str(i), parameters=parameters)
-
-                bucket_name = config.get_csv_bucket_name()
-                response = s3_client.list_objects_v2(
-                    Bucket=bucket_name, Prefix="998/temp/"
-                )
-
-                objects = []
-                if "Contents" in response:
-                    for obj in response["Contents"]:
-                        objects.append(obj["Key"])
-                #  in test parquet, only 1 data csv for the provided range and 1 dataschema.csv file for the parquet dataset
-                assert len(objects) == 2
-                assert (
-                    "998/temp/autonomous_underwater_vehicle.parquet/part-3/PARTITION_KEY=2010-11/part.0.parquet"
-                    in objects
-                )
-                assert "998/temp/dataschema.json" in objects
-
-            except Exception as ex:
-                raise ex
-            finally:
-                # Delete temp output folder as the name always same for testing
-                shutil.rmtree(config.get_temp_folder("998"), ignore_errors=True)
-
-    @patch("aodn_cloud_optimised.lib.DataQuery.REGION", REGION)
     def test_parquet_with_date32_type(
         self,
         aws_clients,
@@ -507,3 +436,74 @@ class TestDataGeneration(TestWithS3):
             finally:
                 # Delete temp output folder as the name always same for testing
                 shutil.rmtree(config.get_temp_folder(INIT_JOB_ID), ignore_errors=True)
+
+    @patch("aodn_cloud_optimised.lib.DataQuery.REGION", REGION)
+    def test_non_specified_multi_polygon(
+        self, aws_clients, setup_resources, upload_test_case_to_s3
+    ):
+        # Use a different master_job_id to avoid conflicts with other tests
+        parameters = PREPARATION_PARAMETERS.copy()
+        parameters[Parameters.MASTER_JOB_ID.value] = "998"
+        parameters[Parameters.MULTI_POLYGON.value] = "non-specified"
+
+        s3_client, _, _ = aws_clients
+        config = Config.get_config()
+        config.set_s3_client(s3_client)
+        helper = AWSHelper()
+
+        with patch.object(AWSHelper, "send_email") as mock_send_email:
+            try:
+                # List objects in S3
+                response = s3_client.list_objects_v2(
+                    Bucket=DataQuery.BUCKET_OPTIMISED_DEFAULT,
+                    Prefix=DataQuery.ROOT_PREFIX_CLOUD_OPTIMISED_PATH,
+                    Delimiter="/",
+                )
+
+                folders = [
+                    prefix["Prefix"][len(prefix) - 1 :]
+                    for prefix in response.get("CommonPrefixes", [])
+                    if prefix["Prefix"].endswith(".parquet/")
+                ]
+
+                assert len(folders) == 3
+                assert folders[1] == "animal_acoustic_tracking_delayed_qc.parquet/"
+
+                # Verify DataQuery functionality
+                aodn = DataQuery.GetAodn()
+                metadata: Metadata = aodn.get_metadata()
+                assert (
+                    metadata.metadata_catalog().get(
+                        "animal_acoustic_tracking_delayed_qc.parquet"
+                    )
+                    is not None
+                )
+
+                # prepare data according to the test parameters
+                api = API()
+                api.initialize_metadata()
+                for i in range(5):
+                    prepare_data(api, job_index=str(i), parameters=parameters)
+
+                bucket_name = config.get_csv_bucket_name()
+                response = s3_client.list_objects_v2(
+                    Bucket=bucket_name, Prefix="998/temp/"
+                )
+
+                objects = []
+                if "Contents" in response:
+                    for obj in response["Contents"]:
+                        objects.append(obj["Key"])
+                #  in test parquet, only 1 data csv for the provided range and 1 dataschema.csv file for the parquet dataset
+                assert len(objects) == 2
+                assert (
+                    "998/temp/autonomous_underwater_vehicle.parquet/part-3/PARTITION_KEY=2010-11/part.0.parquet"
+                    in objects
+                )
+                assert "998/temp/dataschema.json" in objects
+
+            except Exception as ex:
+                raise ex
+            finally:
+                # Delete temp output folder as the name always same for testing
+                shutil.rmtree(config.get_temp_folder("998"), ignore_errors=True)
