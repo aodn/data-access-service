1136971c79d6a376d25af82b6e1e902f41c3e0e8
HuaizhiDai <huaizhi.dai@utas.edu.au>
2026-08-18 16:40:29 +1000

    lazy load aodn metadata

--- data_access_service/models/co_data_source/aodn_data_src.py
+++ data_access_service/models/co_data_source/aodn_data_src.py
@@ -14,17 +14,15 @@ class AodnDataSrc(AbstractDataSrc):
     def __init__(self):
         self.name = AODN
         self.__data_src = GetAodn()
-        self.__metadata = self.__data_src.get_metadata()
-        self.__metadata_catalog = self.__metadata.catalog
 
     def get_name(self) -> str:
         return self.name
 
     def get_metadata(self) -> Metadata:
-        return self.__metadata
+        return self.__data_src.get_metadata()
 
     def get_metadata_catalog(self) -> dict:
-        return self.__metadata_catalog
+        return self.__data_src.get_metadata().catalog
 
     def get_dataset(self, dataset_name_with_ext: str) -> DataSource:
         return super().get_dataset(dataset_name_with_ext=dataset_name_with_ext)
