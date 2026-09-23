b5b1953f229b68000c36a311e327b408adabce44
leslieduan <zhonghui.duan@utas.edu.au>
2026-09-23 16:53:46 +1000

    fix testing

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -24,9 +24,7 @@ def _with_tiler(monkeypatch, config, edit):
 
 def test_tiler_root_dir_is_the_datavis_bucket():
     config = Config.get_config(EnvType.TESTING)
-    assert (
-        config.get_tiler_root_dir() == "s3://test-site-snapshot-bucket/tiler_re_layout"
-    )
+    assert config.get_tiler_root_dir() == "s3://test-site-snapshot-bucket/tiler"
     assert config.get_tiler_batch_config().tiler_root_dir == config.get_tiler_root_dir()
 
 
